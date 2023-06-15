a75a679bbb60d896bd86e23f1f7bc6fa130d6aeb
Gowtham1707 <gowthamvgy@gmail.com>
2023-06-15 18:06:36 +0530

    first commit

---
+++ base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import * 
+
+urlpatterns = [
+    path("", index, name = "index"),
+    path("signup/", signup, name = "signup"),
+    path("signin/",signin, name="signin"),
+    path("logout/",logout, name = "logout"),
+    path("settings/",settings,name="settings"),
+    path('upload/', upload ,name="upload"),
+    path("like-post/",like_post,name='like-post'),
+    path("profile/<str:pk>",profile,name='profile'),
+    path("follow/",follow,name="follow"),
+    path("search/",search,name="search"),
+]
